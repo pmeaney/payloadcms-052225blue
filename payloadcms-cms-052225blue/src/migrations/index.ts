91409fa2ee13b4324062652b03ff1d2ab5dd6b5b
Patrick Meaney <patrick.wm.meaney@gmail.com>
2025-05-23 14:01:42 -0700

    ok things seem to work.  sync from prod works...

---
+++ payloadcms-cms-052225blue/src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250523_184732 from './20250523_184732';
+
+export const migrations = [
+  {
+    up: migration_20250523_184732.up,
+    down: migration_20250523_184732.down,
+    name: '20250523_184732'
+  },
+];
